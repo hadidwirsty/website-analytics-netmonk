708092454b5bb13b1af1a4f2c2629b2d505da1bf
Muhammad Hadid Wiransetyo <hadidwirsty@Muhammads-MacBook-Air.local>
2024-02-19 16:52:03 +0700

    backend_express:fix

--- backend_express/server.js
+++ backend_express/server.js
@@ -1,38 +1,41 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-
 const PORT = process.env.PORT || 3500;
 
-// Custom middleware logger
+// custom middleware logger
 app.use(logger);
 
-// Cors origin resource sharing
+// Cross Origin Resource Sharing
 const whitelist = ['http://127.0.0.1:3500', 'http://localhost:3500'];
-const corsOption = {
+const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by Cors'));
+      callback(new Error('Not allowed by CORS'));
     }
   },
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
 };
-app.use(cors(corsOption));
+app.use(cors(corsOptions));
 
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// ‘content-type: application/x-www-form-urlencoded’
 app.use(express.urlencoded({ extended: false }));
 
+// built-in middleware for json
 app.use(express.json());
 
+//serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname});
+  //res.sendFile('./views/index.html', { root: __dirname });
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
@@ -56,7 +59,7 @@ app.get(
   }
 );
 
-// Chaining route handlers
+// chaining route handlers
 const one = (req, res, next) => {
   console.log('one');
   next();
@@ -78,7 +81,7 @@ app.all('*', (req, res) => {
   res.status(404);
   if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
-  } else if (req.accepts('html')) {
+  } else if (req.accepts('json')) {
     res.json({ error: '404 Not Found' });
   } else {
     res.type('txt').send('404 Not Found');
